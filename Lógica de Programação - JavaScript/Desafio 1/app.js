e03c143e78e525c7df14d5df7e8588c1652e7f2e
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2025-01-16 14:29:32 -0300

    desafio e projeto

---
+++ Lógica de Programação - JavaScript/Desafio 1/app.js
@@ -0,0 +1,8 @@
+let nome = prompt("Seu nome: ")
+let idade = prompt("Sua idade: ")
+
+if(idade >= 18) {
+    alert(`${nome} você é maior de idade e está apto para tirar sua habilitação.`)
+} else {
+    alert(`${nome} você é menor de idade.`)
+}
---
+++ Lógica de Programação - JavaScript/Projeto/app.js
@@ -0,0 +1,7 @@
+alert("Boas vindas ao jogo do número secreto!");
+const numeroSecreto = 6;
+const escolhaDoUsuario = prompt("Escolha um número entre 1 e 10");
+
+if (numeroSecreto == escolhaDoUsuario) {
+    console.log("O número está correto!");
+}
