ec8807d0ae7775365b4b8fb780b8961795a2b0b5
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2025-01-16 14:42:49 -0300

    else

--- Lógica de Programação - JavaScript/Projeto/app.js
+++ Lógica de Programação - JavaScript/Projeto/app.js
@@ -1,7 +1,10 @@
 alert("Boas vindas ao jogo do número secreto!");
 const numeroSecreto = 6;
+console.log(numeroSecreto)
 const escolhaDoUsuario = prompt("Escolha um número entre 1 e 10");
 
 if (numeroSecreto == escolhaDoUsuario) {
-    console.log("O número está correto!");
+    alert("O número está correto! :D");
+} else {
+    alert("Você errou o número secreto :(")
 }
