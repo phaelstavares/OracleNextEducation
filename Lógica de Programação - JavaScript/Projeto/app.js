2afc68285e89a1a74947d1abd8044c692a0cd0b9
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2025-01-17 14:32:27 -0300

    while

--- Lógica de Programação - JavaScript/Projeto/app.js
+++ Lógica de Programação - JavaScript/Projeto/app.js
@@ -1,10 +1,24 @@
 alert("Boas vindas ao jogo do número secreto!");
 const numeroSecreto = 6;
-console.log(numeroSecreto)
-const escolhaDoUsuario = prompt("Escolha um número entre 1 e 10");
+console.log(numeroSecreto);
+let escolhaDoUsuario;
+let tentativas = 1;
 
-if (numeroSecreto == escolhaDoUsuario) {
-    alert(`Você acertou! O número é ${numeroSecreto} :D`);
-} else {
-    alert("Você errou o número secreto :(")
+// enquanto a escolha não for igual ao número secreto
+while (escolhaDoUsuario != numeroSecreto) {
+    escolhaDoUsuario = prompt("Escolha um número entre 1 e 10");
+
+    // se a escolha for igual ao número secreto
+    if (numeroSecreto == escolhaDoUsuario) {
+        alert(`Você acertou! O número é ${numeroSecreto} com ${tentativas} tentativas.`);
+    } else {
+        if (numeroSecreto > escolhaDoUsuario) {
+            alert(`O número secreto é maior que ${escolhaDoUsuario}`);
+        } else {
+            alert(`O número secreto é menor que ${escolhaDoUsuario}`);
+        }
+
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
