cb723e55b93041287c3818b6a56654a908fe24de
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2025-01-20 14:58:28 -0300

    tipo boolean

--- Lógica de Programação - Funções e Listas/Projeto/app.js
+++ Lógica de Programação - Funções e Listas/Projeto/app.js
@@ -9,6 +9,7 @@ exibirTextoNaTela("h1", "Jogo do número secreto");
 exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function verificarChute() {
+    let chute = document.querySelector("input").value;
     console.log("O botão foi clicado!");
 }
 
